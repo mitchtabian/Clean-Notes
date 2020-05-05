64e864ebc593e2e329bd0d654739250e076748e0
mitch <mitch@tabian.ca>
2020-05-05 15:17:59 -0700

    implement /data/cache/

---
+++ app/src/main/java/com/codingwithmitch/cleannotes/business/data/cache/CacheResponseHandler.kt
@@ -0,0 +1,47 @@
+package com.codingwithmitch.cleannotes.business.data.cache
+
+import com.codingwithmitch.cleannotes.business.data.cache.CacheErrors.CACHE_DATA_NULL
+import com.codingwithmitch.cleannotes.business.domain.state.*
+
+
+abstract class CacheResponseHandler <ViewState, Data>(
+    private val response: CacheResult<Data?>,
+    private val stateEvent: StateEvent?
+){
+    suspend fun getResult(): DataState<ViewState>? {
+
+        return when(response){
+
+            is CacheResult.GenericError -> {
+                DataState.error(
+                    response = Response(
+                        message = "${stateEvent?.errorInfo()}\n\nReason: ${response.errorMessage}",
+                        uiComponentType = UIComponentType.Dialog(),
+                        messageType = MessageType.Error()
+                    ),
+                    stateEvent = stateEvent
+                )
+            }
+
+            is CacheResult.Success -> {
+                if(response.value == null){
+                    DataState.error(
+                        response = Response(
+                            message = "${stateEvent?.errorInfo()}\n\nReason: ${CACHE_DATA_NULL}.",
+                            uiComponentType = UIComponentType.Dialog(),
+                            messageType = MessageType.Error()
+                        ),
+                        stateEvent = stateEvent
+                    )
+                }
+                else{
+                    handleSuccess(resultObj = response.value)
+                }
+            }
+
+        }
+    }
+
+    abstract suspend fun handleSuccess(resultObj: Data): DataState<ViewState>?
+
+}
---
+++ app/src/main/java/com/codingwithmitch/cleannotes/business/data/cache/CacheResult.kt
@@ -0,0 +1,10 @@
+package com.codingwithmitch.cleannotes.business.data.cache
+
+sealed class CacheResult<out T> {
+
+    data class Success<out T>(val value: T): CacheResult<T>()
+
+    data class GenericError(
+        val errorMessage: String? = null
+    ): CacheResult<Nothing>()
+}
