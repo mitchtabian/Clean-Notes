090109176503e3dca3651e74b3465266ff86adee
mitch <mitch@tabian.ca>
2020-05-07 10:20:04 -0700

    Add DeleteNote.kt

--- app/src/main/java/com/codingwithmitch/cleannotes/business/interactors/common/DeleteNote.kt
+++ app/src/main/java/com/codingwithmitch/cleannotes/business/interactors/common/DeleteNote.kt
@@ -1,4 +1,82 @@
 package com.codingwithmitch.cleannotes.business.interactors.common
 
-class DeleteNote {
+import com.codingwithmitch.cleannotes.business.data.cache.CacheResponseHandler
+import com.codingwithmitch.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
+import com.codingwithmitch.cleannotes.business.data.network.abstraction.NoteNetworkDataSource
+import com.codingwithmitch.cleannotes.business.domain.model.Note
+import com.codingwithmitch.cleannotes.business.domain.state.*
+import com.codingwithmitch.cleannotes.business.data.util.safeApiCall
+import com.codingwithmitch.cleannotes.business.data.util.safeCacheCall
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class DeleteNote<ViewState>(
+    private val noteCacheDataSource: NoteCacheDataSource,
+    private val noteNetworkDataSource: NoteNetworkDataSource
+){
+
+    fun deleteNote(
+        note: Note,
+        stateEvent: StateEvent
+    ): Flow<DataState<ViewState>?> = flow {
+
+        val cacheResult = safeCacheCall(IO){
+            noteCacheDataSource.deleteNote(note.id)
+        }
+
+        val response = object: CacheResponseHandler<ViewState, Int>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ){
+            override suspend fun handleSuccess(resultObj: Int): DataState<ViewState>? {
+                return if(resultObj > 0){
+                    DataState.data(
+                        response = Response(
+                            message = DELETE_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.None(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+                else{
+                    DataState.data(
+                        response = Response(
+                            message = DELETE_NOTE_FAILED,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+        }.getResult()
+
+        emit(response)
+
+        // update network
+        if(response?.stateMessage?.response?.message.equals(DELETE_NOTE_SUCCESS)){
+
+            // delete from 'notes' node
+            safeApiCall(IO){
+                noteNetworkDataSource.deleteNote(note.id)
+            }
+
+            // insert into 'deletes' node
+            safeApiCall(IO){
+                noteNetworkDataSource.insertDeletedNote(note)
+            }
+
+        }
+    }
+
+    companion object{
+        val DELETE_NOTE_SUCCESS = "Successfully deleted note."
+        val DELETE_NOTE_PENDING = "Delete pending..."
+        val DELETE_NOTE_FAILED = "Failed to delete note."
+        val DELETE_ARE_YOU_SURE = "Are you sure you want to delete this?"
+    }
 }
