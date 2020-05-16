c342c42c93cff08451e6318c2e83b84753059309
mitch <mitch@tabian.ca>
2020-05-16 10:40:09 -0700

    Fix DeleteMultipleNotesTest.kt logic. Two of the tests were not correct.

--- app/src/test/java/com/codingwithmitch/cleannotes/business/interactors/notelist/DeleteMultipleNotesTest.kt
+++ app/src/test/java/com/codingwithmitch/cleannotes/business/interactors/notelist/DeleteMultipleNotesTest.kt
@@ -5,19 +5,20 @@ import com.codingwithmitch.cleannotes.business.data.cache.abstraction.NoteCacheD
 import com.codingwithmitch.cleannotes.business.data.network.abstraction.NoteNetworkDataSource
 import com.codingwithmitch.cleannotes.business.domain.model.Note
 import com.codingwithmitch.cleannotes.business.domain.model.NoteFactory
+import com.codingwithmitch.cleannotes.business.domain.state.DataState
 import com.codingwithmitch.cleannotes.business.interactors.notelist.DeleteMultipleNotes.Companion.DELETE_NOTES_ERRORS
 import com.codingwithmitch.cleannotes.business.interactors.notelist.DeleteMultipleNotes.Companion.DELETE_NOTES_SUCCESS
-import com.codingwithmitch.cleannotes.business.domain.state.DataState
 import com.codingwithmitch.cleannotes.di.DependencyContainer
+import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListStateEvent
 import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListStateEvent.*
 import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListViewState
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Before
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -30,15 +31,15 @@ Test cases:
     a) select a handful of random notes for deleting
     b) delete from cache and network
     c) confirm DELETE_NOTES_SUCCESS msg is emitted from flow
-    d) confirm notes are deleted from network "notes" node
-    e) confirm notes are inserted into network "deletes" node
-    f) confirm notes are deleted from cache
+    d) confirm notes are delted from cache
+    e) confirm notes are deleted from "notes" node in network
+    f) confirm notes are added to "deletes" node in network
 2. deleteNotes_fail_confirmCorrectDeletesMade()
     - This is a complex one:
         - The use-case will attempt to delete all notes passed as input. If there
         is an error with a particular delete, it continues with the others. But the
         resulting msg is DELETE_NOTES_ERRORS. So we need to do rigorous checks here
-        to make sure the correct notes were deleted and the correct notes were note.
+        to make sure the correct notes were deleted and the correct notes were not.
     a) select a handful of random notes for deleting
     b) change the ids of a few notes so they will cause errors when deleting
     c) confirm DELETE_NOTES_ERRORS msg is emitted from flow
@@ -56,8 +57,9 @@ Test cases:
 @InternalCoroutinesApi
 class DeleteMultipleNotesTest {
 
+
     // system in test
-    private var deleteNotes: DeleteMultipleNotes? = null
+    private var deleteMultipleNotes: DeleteMultipleNotes? = null
 
     // dependencies
     private lateinit var dependencyContainer: DependencyContainer
@@ -67,30 +69,27 @@ class DeleteMultipleNotesTest {
 
     @AfterEach
     fun afterEach(){
-        deleteNotes = null
+        deleteMultipleNotes = null
     }
 
     @BeforeEach
     fun beforeEach(){
-        // recreate the dataset before each test
         dependencyContainer = DependencyContainer()
         dependencyContainer.build()
-
         noteCacheDataSource = dependencyContainer.noteCacheDataSource
         noteNetworkDataSource = dependencyContainer.noteNetworkDataSource
         noteFactory = dependencyContainer.noteFactory
-        deleteNotes = DeleteMultipleNotes(
+        deleteMultipleNotes = DeleteMultipleNotes(
             noteCacheDataSource = noteCacheDataSource,
             noteNetworkDataSource = noteNetworkDataSource
         )
     }
 
     @Test
-    fun deleteNotes_success_confirmNetworkAndCacheUpdated() =  runBlocking {
+    fun deleteNotes_success_confirmNetworkAndCacheUpdated() = runBlocking {
 
-        // select a bunch of random notes from cache to delete
         val randomNotes: ArrayList<Note> = ArrayList()
-        val notesInCache = noteCacheDataSource.searchNotes("", "", 1)
+        val notesInCache = noteCacheDataSource.searchNotes("","",1)
 
         for(note in notesInCache){
             randomNotes.add(note)
@@ -99,11 +98,12 @@ class DeleteMultipleNotesTest {
             }
         }
 
-        deleteNotes?.deleteNotes(
+        deleteMultipleNotes?.deleteNotes(
             notes = randomNotes,
             stateEvent = DeleteMultipleNotesEvent(randomNotes)
-        )?.collect(object: FlowCollector<DataState<NoteListViewState>?> {
+        )?.collect(object: FlowCollector<DataState<NoteListViewState>?>{
             override suspend fun emit(value: DataState<NoteListViewState>?) {
+
                 assertEquals(
                     value?.stateMessage?.response?.message,
                     DELETE_NOTES_SUCCESS
@@ -111,32 +111,35 @@ class DeleteMultipleNotesTest {
             }
         })
 
-        // confirm notes were inserted into "deletes" node
-        val deletedNetworkNotes = noteNetworkDataSource.getDeletedNotes()
-        assertTrue { deletedNetworkNotes.containsAll(randomNotes) }
 
-        // confirm notes were deleted from "notes" node
-        val doNotesExistInNetwork = noteNetworkDataSource.getAllNotes().containsAll(randomNotes)
-        assertFalse { doNotesExistInNetwork }
-
-        // confirm notes were deleted from cache
+        // confirm notes are delted from cache
         for(note in randomNotes){
             val noteInCache = noteCacheDataSource.searchNoteById(note.id)
-            assertTrue { noteInCache == null }
+            assertTrue {noteInCache == null}
         }
+
+        // confirm notes are deleted from "notes" node in network
+        val doNotesExistInNetwork = noteNetworkDataSource.getAllNotes()
+            .containsAll(randomNotes)
+        assertFalse {doNotesExistInNetwork}
+
+
+        // confirm notes are added to "deletes" node in network
+        val deletedNetworkNotes = noteNetworkDataSource.getDeletedNotes()
+        assertTrue { deletedNetworkNotes.containsAll(randomNotes)}
+
     }
 
-    @Test
-    fun deleteNotes_fail_confirmCorrectDeletesMade() =  runBlocking {
 
-        // select a bunch of random notes from cache to delete
-        // change the ids of a few to force errors
+    @Test
+    fun deleteNotes_fail_confirmCorrectDeletesMade() = runBlocking {
+
         val validNotes: ArrayList<Note> = ArrayList()
         val invalidNotes: ArrayList<Note> = ArrayList()
         val notesInCache = noteCacheDataSource.searchNotes("", "", 1)
         for(index in 0..notesInCache.size){
             var note: Note
-            if(index % 2 == 0){ // every even index create an error
+            if(index % 2 == 0){
                 note = noteFactory.createSingleNote(
                     id = UUID.randomUUID().toString(),
                     title = notesInCache.get(index).title,
@@ -152,12 +155,14 @@ class DeleteMultipleNotesTest {
                 break
             }
         }
+
         val notesToDelete = ArrayList(validNotes + invalidNotes)
-        deleteNotes?.deleteNotes(
+        deleteMultipleNotes?.deleteNotes(
             notes = notesToDelete,
             stateEvent = DeleteMultipleNotesEvent(notesToDelete)
-        )?.collect(object: FlowCollector<DataState<NoteListViewState>?> {
+        )?.collect(object: FlowCollector<DataState<NoteListViewState>?>{
             override suspend fun emit(value: DataState<NoteListViewState>?) {
+
                 assertEquals(
                     value?.stateMessage?.response?.message,
                     DELETE_NOTES_ERRORS
@@ -165,32 +170,32 @@ class DeleteMultipleNotesTest {
             }
         })
 
+
         // confirm ONLY the valid notes are deleted from network "notes" node
-        val deletedNetworkNotes = noteNetworkDataSource.getDeletedNotes()
-        assertTrue { deletedNetworkNotes.containsAll(validNotes) }
-        assertTrue { !deletedNetworkNotes.containsAll(invalidNotes) }
+        val networkNotes = noteNetworkDataSource.getAllNotes()
+        assertFalse { networkNotes.containsAll(validNotes)}
 
         // confirm ONLY the valid notes are inserted into network "deletes" node
-        val doValidNotesExistInNetwork = noteNetworkDataSource.getDeletedNotes().containsAll(validNotes)
-        val doInvalidNotesExistInNetwork = noteNetworkDataSource.getAllNotes().containsAll(invalidNotes)
-        assertTrue { doValidNotesExistInNetwork }
-        assertTrue { !doInvalidNotesExistInNetwork }
+        val deletedNetworkNotes = noteNetworkDataSource.getDeletedNotes()
+        assertTrue { deletedNetworkNotes.containsAll(validNotes) }
+        assertFalse { deletedNetworkNotes.containsAll(invalidNotes) }
+
 
         // confirm ONLY the valid notes are deleted from cache
         for(note in validNotes){
             val noteInCache = noteCacheDataSource.searchNoteById(note.id)
-            assertTrue { noteInCache == null }
+            assertTrue {noteInCache == null}
         }
         val numNotesInCache = noteCacheDataSource.getNumNotes()
         assertTrue { numNotesInCache == (notesInCache.size - validNotes.size) }
+
     }
 
     @Test
-    fun throwException_checkGenericError_confirmNetworkAndCacheUnchanged() = runBlocking{
+    fun throwException_checkGenericError_confirmNetworkAndCacheUnchanged() = runBlocking {
 
-        // select a bunch of random notes from cache to delete
-        val invalidNotes: ArrayList<Note> = ArrayList()
         val validNotes: ArrayList<Note> = ArrayList()
+        val invalidNotes: ArrayList<Note> = ArrayList()
         val notesInCache = noteCacheDataSource.searchNotes("", "", 1)
         for(note in notesInCache){
             validNotes.add(note)
@@ -199,22 +204,22 @@ class DeleteMultipleNotesTest {
             }
         }
 
-        // create a note to delete that will throw exception
         val errorNote = Note(
-            id = FORCE_DELETE_NOTE_EXCEPTION ,
+            id = FORCE_DELETE_NOTE_EXCEPTION,
             title = UUID.randomUUID().toString(),
             body = UUID.randomUUID().toString(),
-            updated_at = UUID.randomUUID().toString(),
-            created_at = UUID.randomUUID().toString()
+            created_at = UUID.randomUUID().toString(),
+            updated_at = UUID.randomUUID().toString()
         )
         invalidNotes.add(errorNote)
 
-        val notesToDelete = ArrayList(invalidNotes + validNotes)
-        deleteNotes?.deleteNotes(
-            notesToDelete,
-            DeleteMultipleNotesEvent(notesToDelete)
+        val notesToDelete = ArrayList(validNotes + invalidNotes)
+        deleteMultipleNotes?.deleteNotes(
+            notes = notesToDelete,
+            stateEvent = DeleteMultipleNotesEvent(notesToDelete)
         )?.collect(object: FlowCollector<DataState<NoteListViewState>?>{
             override suspend fun emit(value: DataState<NoteListViewState>?) {
+
                 assertEquals(
                     value?.stateMessage?.response?.message,
                     DELETE_NOTES_ERRORS
@@ -222,27 +227,57 @@ class DeleteMultipleNotesTest {
             }
         })
 
+
         // confirm ONLY the valid notes are deleted from network "notes" node
-        val deletedNetworkNotes = noteNetworkDataSource.getDeletedNotes()
-        assertTrue { deletedNetworkNotes.containsAll(validNotes) }
-        assertTrue { !deletedNetworkNotes.containsAll(invalidNotes) }
+        val networkNotes = noteNetworkDataSource.getAllNotes()
+        assertFalse { networkNotes.containsAll(validNotes)}
 
         // confirm ONLY the valid notes are inserted into network "deletes" node
-        val doValidNotesExistInNetwork = noteNetworkDataSource.getDeletedNotes().containsAll(validNotes)
-        val doInvalidNotesExistInNetwork = noteNetworkDataSource.getAllNotes().containsAll(invalidNotes)
-        assertTrue { doValidNotesExistInNetwork }
-        assertTrue { !doInvalidNotesExistInNetwork }
+        val deletedNetworkNotes = noteNetworkDataSource.getDeletedNotes()
+        assertTrue { deletedNetworkNotes.containsAll(validNotes) }
+        assertFalse { deletedNetworkNotes.containsAll(invalidNotes) }
+
 
         // confirm ONLY the valid notes are deleted from cache
         for(note in validNotes){
             val noteInCache = noteCacheDataSource.searchNoteById(note.id)
-            assertTrue { noteInCache == null }
+            assertTrue {noteInCache == null}
         }
         val numNotesInCache = noteCacheDataSource.getNumNotes()
         assertTrue { numNotesInCache == (notesInCache.size - validNotes.size) }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
