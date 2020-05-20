2d687d93eff7d1f76ccf0469665e3a6ed85584bc
mitch <mitch@tabian.ca>
2020-05-20 11:28:45 -0700

    Add Firebase setup

--- app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/BaseApplication.kt
+++ app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/BaseApplication.kt
@@ -1,11 +1,24 @@
 package com.codingwithmitch.cleannotes.framework.presentation
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
+import kotlinx.coroutines.*
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 open class BaseApplication : Application(){
+
 
     override fun onCreate() {
         super.onCreate()
+        initCrashlytics()
+    }
+
+    private fun initCrashlytics(){
+        val core = CrashlyticsCore.Builder().disabled(false).build()
+        Fabric.with(this, Crashlytics.Builder().core(core).build())
     }
 
 
