e42695622cbe93bc71899e051ccea11e4eb10d26
mitch <mitch@tabian.ca>
2020-05-21 14:03:17 -0700

    cleanup BaseApplication.

--- app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/BaseApplication.kt
+++ app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/BaseApplication.kt
@@ -1,9 +1,6 @@
 package com.codingwithmitch.cleannotes.framework.presentation
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
-import io.fabric.sdk.android.Fabric
 import kotlinx.coroutines.*
 
 @FlowPreview
@@ -13,13 +10,9 @@ open class BaseApplication : Application(){
 
     override fun onCreate() {
         super.onCreate()
-        initCrashlytics()
     }
 
-    private fun initCrashlytics(){
-        val core = CrashlyticsCore.Builder().disabled(false).build()
-        Fabric.with(this, Crashlytics.Builder().core(core).build())
-    }
+
 
 
 }
